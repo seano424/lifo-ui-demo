580045e2a2afc34fb865413e39b7db07cfa683bd
seano424 <soreilly424@gmail.com>
2025-06-19 12:19:32 -0600

    Update ESLint configuration to use object syntax for plugins, enhancing compatibility with Prettier integration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-plugin-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,7 +14,9 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: ['prettier'],
+    plugins: {
+      prettier: prettier,
+    },
     rules: {
       'prettier/prettier': 'error',
       'arrow-body-style': 'off',
