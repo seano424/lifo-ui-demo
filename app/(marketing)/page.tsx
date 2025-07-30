792bd4551f7f6cfa29fa6ee8e7a1e95753a73473
Félix Orain <felix.orain@gmail.com>
2025-07-30 17:22:53 +0200

    🎨 Frontend features-sumamry section - Adding the features-summary component to improve the presentation of functionalities and optimization of the structure of the home page.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,17 +1,19 @@
-import { hasEnvVars } from '@/lib/utils'
 import { Hero } from '@/components/hero'
-import { Typography } from '@/components/ui/typography'
+import { FeaturesSummary } from '@/components/marketing/features-summary'
 import { ThemeSwitcher } from '@/components/theme-switcher'
-import { SignUpUserSteps } from '@/components/tutorial/sign-up-user-steps'
 import { ConnectSupabaseSteps } from '@/components/tutorial/connect-supabase-steps'
+import { SignUpUserSteps } from '@/components/tutorial/sign-up-user-steps'
 import { LanguageSwitcher } from '@/components/ui/language-switcher'
+import { Typography } from '@/components/ui/typography'
+import { hasEnvVars } from '@/lib/utils'
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+        <div className="flex-1 flex flex-col gap-12 max-w-5xl p-5">
           <Hero />
+          <FeaturesSummary />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <Typography variant="h2">Next steps</Typography>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
