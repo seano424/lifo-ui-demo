33126ab54de152f115c5c49a02d050eb3981965a
Félix Orain <felix.orain@gmail.com>
2025-07-30 19:23:49 +0200

    🎨 Frontend - Adding the businessstats component to display commercial statistics on the home page.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from '@/components/hero'
+import { BusinessStats } from '@/components/marketing/business-stats'
 import { FeaturesSummary } from '@/components/marketing/features-summary'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { ConnectSupabaseSteps } from '@/components/tutorial/connect-supabase-steps'
@@ -14,6 +15,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col gap-12 max-w-5xl p-5">
           <Hero />
           <FeaturesSummary />
+          <BusinessStats />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <Typography variant="h2">Next steps</Typography>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
