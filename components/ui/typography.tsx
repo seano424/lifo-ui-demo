752c00ef963d9fdb65581e229b4e7a41ae03f452
seano424 <soreilly424@gmail.com>
2026-01-29 13:22:52 -0700

    refactor: update opacity levels in marketing page gradient backgrounds
    - Reduced opacity for gradient blobs to create a softer visual effect.
    - Enhanced the overall aesthetic by adjusting opacity values for better integration with the page design.
    - Maintained existing animation properties to ensure fluidity in visual transitions.

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -7,9 +7,9 @@ import { cn } from '@/lib/utils'
 const typographyVariants = cva('', {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl sm:text-5xl font-bold font-heading',
-      h2: 'scroll-m-20 text-3xl sm:text-4xl font-bold font-heading',
-      h3: 'scroll-m-20 text-2xl lg:text-3xl font-bold font-heading',
+      h1: 'scroll-m-20 text-4xl sm:text-5xl font-black font-heading',
+      h2: 'scroll-m-20 text-3xl sm:text-4xl font-black font-heading',
+      h3: 'scroll-m-20 text-2xl lg:text-3xl font-black font-heading',
       h4: 'scroll-m-20 text-xl lg:text-2xl font-medium font-heading',
       h5: 'scroll-m-20 text-lg lg:text-xl font-semibold font-heading',
       p: 'text-base leading-5 font-sans',
