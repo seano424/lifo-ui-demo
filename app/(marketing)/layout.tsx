3ffec70cf6a613ee347c909d43414dcb2fe9fe88
seano424 <soreilly424@gmail.com>
2026-01-30 11:02:54 -0700

    revert: restore marketing layout to main branch state
    Reverted app/(marketing)/layout.tsx to match main branch.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,6 +4,7 @@ import { PostHogProvider } from '@/app/providers/posthog-provider'
 import { CookieConsentBanner } from '@/components/cookie-consent-banner'
 import { MarketingFooter } from '@/components/marketing/marketing-footer'
 import { MarketingNav } from '@/components/marketing/marketing-nav'
+import { GridBackground } from '@/components/ui/grid-background'
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,10 +14,14 @@ export default function MarketingLayout({ children }: { children: React.ReactNod
       </Suspense>
       <CookieConsentBanner />
       <div className="relative">
-        <div className="h-20 py-4 px-4 absolute top-0 left-0 right-0 z-50 flex flex-col justify-center">
+        <GridBackground gridSize={40} transparencyLevel="medium" dotSize={1.5} />
+
+        <div className="h-20 border-b border-foreground/10 py-4 px-4 fixed top-0 left-0 right-0 z-50 flex flex-col justify-center bg-background ">
           <MarketingNav />
         </div>
-        <div className="relative z-10">{children}</div>
+
+        <div className="relative top-20 z-10">{children}</div>
+
         <MarketingFooter />
       </div>
     </PostHogProvider>
