1dc1a42af476197ac8bce7e479cc7589afcbe084
seano424 <soreilly424@gmail.com>
2026-02-14 08:50:11 -0700

    style: update authentication pages for consistent layout
    - Adjusted the layout of the forgot password, login, and sign-up pages to use `lg:min-h-svh` for improved responsiveness.
    - Removed unnecessary `md:p-10` padding for a cleaner design.
    
    These changes enhance the visual consistency and user experience across authentication pages.

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form'
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex lg:min-h-svh w-full items-center justify-center p-6">
       <LoginForm />
     </div>
   )
